64cec1cd995f4a852e09f26fd81748055998351b
CaioHVectorA <caihebatista@gmail.com>
2024-11-22 00:04:35 -0300

    Enhance Tailwind configuration with custom font and update package.json with new dependencies and script modifications

--- apps/www/tailwind.config.ts
+++ apps/www/tailwind.config.ts
@@ -17,7 +17,10 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		fontFamily: {
+			syne: 'var(--font-syne)'
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
