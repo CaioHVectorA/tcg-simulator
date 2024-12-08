87d28fd20f68022d72368b846d93f184b2134615
CaioHVectorA <caihebatista@gmail.com>
2024-12-08 19:17:19 -0300

    Enhance: add guest user creation endpoint and improve response structure in auth controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -8,7 +8,7 @@ const baseResponse = t.Object({
   ok: t.Boolean(),
   toast: t.String(),
   error: t.Union([t.String(), t.Null()]),
-  data: t.Any()
+  data: t.Any(),
 });
 
 export const authController = new Elysia({}).group("/auth", (app) => {
@@ -20,17 +20,17 @@ export const authController = new Elysia({}).group("/auth", (app) => {
       async ({ body, jwt, set }) => {
         const { email, password } = body;
         const user = await prisma.user.findFirst({ where: { email } });
-        
+
         if (!user) {
           set.status = 404;
           return {
             ok: false,
             toast: "Usuário não encontrado",
             error: "Usuário não encontrado",
-            data: null
+            data: null,
           };
         }
-        
+
         const isValid = await compare(password, user.password);
         if (!isValid) {
           set.status = 400;
@@ -38,18 +38,18 @@ export const authController = new Elysia({}).group("/auth", (app) => {
             ok: false,
             toast: "Senha inválida",
             error: "Senha inválida",
-            data: null
+            data: null,
           };
         }
 
         const token = await jwt.sign({ id: user.id });
         console.log({ token, verify: await jwt.verify(token) });
-        
+
         return {
           ok: true,
           toast: "Login efetuado com sucesso!",
           error: null,
-          data: { token }
+          data: { token },
         };
       },
       {
@@ -58,8 +58,8 @@ export const authController = new Elysia({}).group("/auth", (app) => {
         response: {
           200: baseResponse,
           400: baseResponse,
-          404: baseResponse
-        }
+          404: baseResponse,
+        },
       }
     )
     .post(
@@ -67,14 +67,14 @@ export const authController = new Elysia({}).group("/auth", (app) => {
       async ({ body, jwt, set }) => {
         const { email, password, username } = body;
         const alreadyExists = await prisma.user.findFirst({ where: { email } });
-        
+
         if (alreadyExists) {
           set.status = 400;
           return {
             ok: false,
             toast: "O usuário já existe!",
             error: "O usuário já existe!",
-            data: null
+            data: null,
           };
         }
 
@@ -82,13 +82,13 @@ export const authController = new Elysia({}).group("/auth", (app) => {
         const user = await prisma.user.create({
           data: { email, password: hashed, username },
         });
-        
+
         const token = await jwt.sign({ id: user.id });
         return {
           ok: true,
           toast: "Usuário criado com sucesso!",
           error: null,
-          data: { token }
+          data: { token },
         };
       },
       {
@@ -100,8 +100,34 @@ export const authController = new Elysia({}).group("/auth", (app) => {
         detail: { tags: ["Auth"] },
         response: {
           200: baseResponse,
-          400: baseResponse
-        }
+          400: baseResponse,
+        },
       }
-    );
+    )
+    .post("/guest", async ({ body, jwt }) => {
+      let count = await prisma.user.count({
+        where: { username: { startsWith: "Convidado" } },
+      });
+      let name = `Convidado ${Math.floor(Math.random() * 1000 + count)}`;
+      while (true) {
+        const user = await prisma.user.findFirst({ where: { username: name } });
+        if (!user) break;
+        name = `Convidado ${Math.floor(Math.random() * 1000 + count)}`;
+      }
+      const newUser = await prisma.user.create({
+        data: {
+          username: name,
+          email: `${name.replace(" ", "").toLowerCase()}@simtcg.com`,
+          password: await hash((Math.random() * 100_000_000).toFixed(6), 10),
+        },
+        select: { id: true },
+      });
+      const token = await jwt.sign({ id: newUser.id });
+      return {
+        ok: true,
+        toast: "Usuário convidado criado com sucesso!",
+        error: null,
+        data: { token },
+      };
+    });
 });
