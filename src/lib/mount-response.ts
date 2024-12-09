fa51a60bf69a49b728b3bb5fe59ebfb9d097e858
CaioHVectorA <caihebatista@gmail.com>
2024-12-08 23:54:06 -0300

    Add: implement success and error response utility functions, to handle better the response

---
+++ src/lib/mount-response.ts
@@ -0,0 +1,30 @@
+type ResponseMountedOptions<T> = {
+  ok?: boolean;
+  toast?: string | null;
+  data?: T | null;
+  error?: string | null;
+};
+
+export const sucessResponse = <T>(
+  data: T,
+  toast?: string
+): ResponseMountedOptions<T> => {
+  return {
+    ok: true,
+    toast: toast || null,
+    data,
+    error: null,
+  };
+};
+
+export const errorResponse = <T>(
+  error: string,
+  toast?: string
+): ResponseMountedOptions<T> => {
+  return {
+    ok: false,
+    toast: toast || null,
+    data: null,
+    error,
+  };
+};
