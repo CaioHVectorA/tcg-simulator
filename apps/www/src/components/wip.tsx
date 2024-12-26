5c6c1cca7f847e38a61ab35b83c56c5314b0fe23
CaioHVectorA <caihebatista@gmail.com>
2024-12-26 16:33:24 -0300

    feat: add navigation button to Wip component for improved user experience

--- apps/www/src/components/wip.tsx
+++ apps/www/src/components/wip.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link"
+import { Button } from "./ui/button"
+
 export function Wip() {
     return (
         <div className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 text-black text-center p-4">
@@ -15,6 +18,11 @@ export function LargeWip() {
             <img src="/wip.png" alt="TCG Logo" className="rounded-full max-w-[40%] object-cover mx-auto" />
             <h1 className="text-2xl font-bold">Funcionalidade em construção</h1>
             <p className="mt-2">Estamos trabalhando nela, volte mais tarde!</p>
-        </div>
+            <Button asChild className="mt-4">
+                <Link href="/home">
+                    Voltar para a Home
+                </Link>
+            </Button>
+        </div >
     )
 }
