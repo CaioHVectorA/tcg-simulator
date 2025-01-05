687abacf861e97f43466eb3dcf65cc8532d87d2f
CaioHVectorA <caihebatista@gmail.com>
2025-01-05 16:39:03 -0300

    feat: implement create and redeem referral functionality in referralController

---
+++ apps/www/src/app/(app)/afiliado/page.tsx
@@ -0,0 +1,174 @@
+"use client"
+
+import { LoadingRing } from "@/components/loading-spinner"
+import { Button } from "@/components/ui/button"
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { useApi } from "@/hooks/use-api"
+import { useToast } from "@/hooks/use-toast"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import ReactNiceAvatar, { genConfig } from "react-nice-avatar"
+type Referral = {
+    hash: string;
+    referrals: {
+        id: number;
+        referredId: number;
+        referrerProtocolId: number;
+        userId: number;
+        username: string;
+        email: string;
+        referredDate: string;
+        referredRarity: number;
+        redeemed: boolean;
+    }[];
+}
+
+function TableReferral({ referrals }: { referrals: Referral['referrals'] }) {
+    const { post } = useApi()
+    const qClient = useQueryClient()
+    const { mutate } = useMutation({
+        mutationFn: async (referredId: number) => {
+            const response = await post('/referral/redeem', { referredId })
+            await qClient.invalidateQueries({ queryKey: ["referral", "my-referrals"] })
+            await qClient.invalidateQueries({ queryKey: ["me"] })
+            await qClient.refetchQueries({ queryKey: ["referral", "my-referrals"] })
+            await qClient.refetchQueries({ queryKey: ["me"] })
+            return response.data.data
+        },
+        mutationKey: ["referral", "redeem"]
+    })
+    return (
+        <div className="h-80 overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 snap-y snap-mandatory">
+            <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50 snap-start">
+                    <tr>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Nome
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Pontos de Raridade
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Ações
+                        </th>
+                    </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200 h-40">
+                    {referrals.map((referral, index) => (
+                        <tr className=" snap-start" key={index}>
+                            {/* <td className="px-6 py-4 whitespace-nowrap"> */}
+                            <td className="px-6 py-4 gap-2 flex items-center whitespace-nowrap">
+                                <ReactNiceAvatar className='h-12 w-12' {...genConfig(referral.username)} />
+                                {/* </td> */}
+                                <div className="text-lg font-syne font-medium text-gray-900">{referral.username}</div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                                <div className="text-sm text-gray-500 font-syne">{referral.referredRarity} Pontos</div>
+                            </td>
+                            {/* <td className="px-6 py-4 whitespace-nowrap">
+                                <div className="text-sm text-gray-500">{new Date(referral.referredDate).toLocaleDateString()}</div>
+                            </td> */}
+                            <td className="px-6 py-4 whitespace-nowrap">
+                                <Button onClick={() => mutate(referral.id)} disabled={referral.redeemed} className="rounded-full font-syne text-sm" size={'sm'}>
+                                    {referral.redeemed ? 'Resgatado' : 'Resgatar'}
+                                </Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default function Affiliate() {
+    const { get, post } = useApi()
+    const qClient = useQueryClient()
+    const { data, isLoading } = useQuery<Referral>({
+        queryKey: ["referral", "my-referrals"],
+        queryFn: async () => {
+            const response = await get('/referral/my-referrals')
+            return response.data.data
+        }
+    })
+    const { mutate } = useMutation({
+        mutationFn: async () => {
+            const response = await post('/referral/create-referral', {})
+            await qClient.invalidateQueries({ queryKey: ["referral", "my-referrals"] })
+            await qClient.refetchQueries({ queryKey: ["referral", "my-referrals"] })
+            return response.data.data
+        },
+        mutationKey: ["referral", "create-referral"]
+    })
+    const { toast } = useToast()
+    const handleCopy = async () => {
+        if (!data) return
+        await navigator.clipboard.writeText(`${window.location.origin}/entrar?with_bonus=true&referrer=${data.hash}`)
+        toast({ title: 'Link copiado!', description: 'Agora você pode compartilhar seu link com seus amigos!' })
+    }
+    return (
+        <main className=" grid grid-cols-2 gap-4 px-[5%]">
+            <div>
+                {!data ? (
+                    <div className=" mt-6 flex flex-col items-center">
+                        <h2 className=" font-syne text-4xl text-right">
+                            Convide seus amigos, ganhe moedas!
+                        </h2>
+                        <h3 className=" font-syne text-3xl text-center">Comece agora!</h3>
+                        <p className="font-syne text-xl text-center mt-2">Está gostando da experiência? Por que não chama seus amigos para colecionar cartas com você?</p>
+                        <Button onClick={() => mutate()} className=" mt-12 rounded-full font-syne text-xl" size={'lg'}>Criar link de afiliado</Button>
+                    </div>
+                ) : (
+                    <div>
+                        <h3 className=" font-syne mt-8 text-3xl text-center">Seu link de afiliado</h3>
+                        <p className=" font-syne text-xl text-center mt-2">Compartilhe este link com seus amigos e comece a ganhar moedas!</p>
+                        <div className=" mt-12 flex">
+                            <input type="text" disabled value={`${window.location.origin}/entrar?with_bonus=true&referrer=${data.hash}`} className=" w-full text-black/50 p-2 border rounded-l-full" />
+                            <Button onClick={handleCopy} className="rounded-r-full font-syne text-xl" size={'lg'}>Copiar</Button>
+                        </div>
+                    </div>
+                )}
+                <img src="/afiliate.png" alt="Dois amigos apertando suas mãos" className="w-64 mx-auto mt-4 object-cover" />
+            </div>
+            {/* <section className=" pt-8 pb-20 flex flex-col items-center justify-center relative"> */}
+            {isLoading ? (
+                <>
+                    <LoadingRing />
+                </>
+            ) : (
+                <>
+                    {!data ? (
+                        <div className=" mt-6 flex flex-col items-center">
+                            <h2 className=" font-syne text-4xl text-right">
+                                Convide seus amigos, ganhe moedas!
+                            </h2>
+                            <h3 className=" font-syne text-3xl text-center">Comece agora!</h3>
+                            <p className="font-syne text-xl text-center mt-2">Está gostando da experiência? Por que não chama seus amigos para colecionar cartas com você?</p>
+                            <Button onClick={() => mutate()} className=" mt-12 rounded-full font-syne text-xl" size={'lg'}>Criar link de afiliado</Button>
+                        </div>
+                    ) : (
+                        <div>
+                            {/* <h3 className=" font-syne mt-8 text-3xl text-center">Seu link de afiliado</h3>
+                                <p className=" font-syne text-xl text-center mt-2">Compartilhe este link com seus amigos e comece a ganhar moedas!</p>
+                                <div className=" mt-12 flex">
+                                    <input type="text" disabled value={`${window.location.origin}/entrar?with_bonus=true&referrer=${data.hash}`} className=" w-full text-black/50 p-2 border rounded-l-full" />
+                                    <Button onClick={handleCopy} className="rounded-r-full font-syne text-xl" size={'lg'}>Copiar</Button>
+                                </div> */}
+                            <h3 className=" font-syne mt-8 text-3xl text-center">Seus convidados</h3>
+                            {data.referrals.length === 0 ? (
+                                <Card className=" mt-4">
+                                    <CardHeader>
+                                        <CardTitle className=" text-xl font-syne">Você ainda não convidou ninguém</CardTitle>
+                                    </CardHeader>
+                                    <CardDescription className=" text-center">Compartilhe seu link de afiliado e comece a ganhar moedas convidando seus amigos!</CardDescription>
+                                </Card>
+                            ) : (
+                                <TableReferral referrals={data.referrals} />
+                            )}
+
+                        </div>
+                    )}
+                </>
+            )}
+            {/* </section> */}
+        </main>
+    )
+}
--- src/controller/referral.controller.ts
+++ src/controller/referral.controller.ts
@@ -1,15 +1,95 @@
 import type { User } from "@prisma/client";
 import { prisma } from "../helpers/prisma.client";
-import { sucessResponse } from "../lib/mount-response";
+import { errorResponse, sucessResponse } from "../lib/mount-response";
 import { getUserUserMiddleware } from "../middlewares/jwt";
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { jwt } from "../middlewares/jwt/jwt";
+import { randomUUID } from "crypto";
 
-export const bannerController = new Elysia({}).group("/referral", (app) => {
+export const referralController = new Elysia({}).group("/referral", (app) => {
   return app
     .decorate("prisma", prisma)
     .use(jwt)
     .decorate("user", {} as User)
     .onBeforeHandle(getUserUserMiddleware as any)
-    .get("/my-referrals", async ({ prisma, user }) => {});
+    .post("/create-referral", async ({ prisma, user }) => {
+      const referralExists = await prisma.referrerProtocol.findFirst({
+        where: { referrerCreatorId: user.id },
+      });
+      if (referralExists) {
+        return sucessResponse(referralExists);
+      }
+      const hash = randomUUID().replaceAll("-", "");
+      const referral = await prisma.referrerProtocol.create({
+        data: { referrerCreatorId: user.id, hash },
+      });
+      return sucessResponse(referral);
+    })
+    .get("/my-referrals", async ({ prisma, user, set }) => {
+      const protocol = await prisma.referrerProtocol.findFirst({
+        where: { referrerCreatorId: user.id },
+      });
+      if (!protocol) {
+        set.status = 404;
+        return errorResponse("Protocolo não encontrado");
+      }
+      const referrals = await prisma.referred.findMany({
+        where: { referrerProtocolId: protocol.id },
+        include: { referred: { include: { User_Ranking: true } } },
+      });
+      const response = {
+        hash: protocol.hash,
+        referrals: referrals.map((referral) => ({
+          id: referral.id,
+          referredId: referral.referredId,
+          referrerProtocolId: referral.referrerProtocolId,
+          userId: referral.referred.id,
+          username: referral.referred.username,
+          email: referral.referred.email,
+          referredDate: referral.createdAt,
+          referredRarity: referral.referred.User_Ranking?.total_rarity || 0,
+          redeemed: referral.bountyRedeemed,
+        })),
+      };
+      return sucessResponse(response);
+    })
+    .post(
+      "/redeem",
+      async ({ prisma, user, body, set }) => {
+        const { referredId } = body;
+        const protocol = await prisma.referrerProtocol.findFirst({
+          where: { referrerCreatorId: user.id },
+        });
+        if (!protocol) {
+          set.status = 404;
+          return errorResponse("Protocolo não encontrado");
+        }
+        const referred = await prisma.referred.findFirst({
+          where: { id: referredId, referrerProtocolId: protocol.id },
+        });
+        if (!referred) {
+          set.status = 404;
+          return errorResponse("Referido não encontrado");
+        }
+        if (referred.bountyRedeemed) {
+          set.status = 400;
+          return errorResponse("Referido já resgatado");
+        }
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { money: { increment: 2000 } },
+        });
+        await prisma.referred.update({
+          where: { id: referred.id },
+          data: { bountyRedeemed: true },
+        });
+        return sucessResponse(
+          "Referido resgatado com sucesso",
+          "Parabéns! Você recebeu 2000 moedas"
+        );
+      },
+      {
+        body: t.Object({ referredId: t.Number() }),
+      }
+    );
 });
