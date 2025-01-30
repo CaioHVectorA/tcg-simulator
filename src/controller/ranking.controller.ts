392584335a61280aa54025c4669ea12b9d190d09
CaioHVectorA <caihebatista@gmail.com>
2025-01-29 21:41:08 -0300

    feat: add monetary ranking endpoint to retrieve user rankings based on monetary contributions

--- src/controller/ranking.controller.ts
+++ src/controller/ranking.controller.ts
@@ -3,40 +3,54 @@ import { prisma } from "../helpers/prisma.client";
 import { sucessResponse } from "../lib/mount-response";
 
 export const rankingController = new Elysia({}).group("/ranking", (app) => {
-  return app.get(
-    "/",
-    async ({ query }) => {
-      const { page } = query;
-      const limit = 10;
-      const offset = (parseInt(page ?? "1") - 1) * limit;
-      const ranking = await prisma.user.findMany({
-        take: Number(limit ?? 10),
-        skip: Number(offset ?? 0),
-        orderBy: {
-          rarityPoints: "desc",
-        },
-        select: {
-          // total_rarity: true,
-          // position: true,
-          // user: {
-          // select: {
-          // username: true,
-          // picture: true,
-          // id: true,
-          // },
-          // },
-          username: true,
-          picture: true,
-          id: true,
-          rarityPoints: true,
-        },
-      });
-      return sucessResponse(ranking);
-    },
-    {
-      query: t.Object({
-        page: t.Optional(t.String()),
-      }),
-    }
-  );
+  return app
+    .get(
+      "/",
+      async ({ query }) => {
+        const { page } = query;
+        const limit = 10;
+        const offset = (parseInt(page ?? "1") - 1) * limit;
+        const ranking = await prisma.user.findMany({
+          take: Number(limit ?? 10),
+          skip: Number(offset ?? 0),
+          orderBy: {
+            rarityPoints: "desc",
+          },
+          select: {
+            // total_rarity: true,
+            // position: true,
+            // user: {
+            // select: {
+            // username: true,
+            // picture: true,
+            // id: true,
+            // },
+            // },
+            username: true,
+            picture: true,
+            id: true,
+            rarityPoints: true,
+          },
+        });
+        return sucessResponse(ranking);
+      },
+      {
+        query: t.Object({
+          page: t.Optional(t.String()),
+        }),
+      }
+    )
+    .get(
+      "/monetary",
+      ({ query }) => {
+        const { page } = query;
+        const limit = 10;
+        const offset = (parseInt(page ?? "1") - 1) * limit;
+      },
+      {
+        query: t.Object({
+          page: t.Optional(t.String()),
+        }),
+      }
+    );
 });
