10f3931696e3c15faff042e1b489eace332aaea0
CaioHVectorA <caihebatista@gmail.com>
2025-02-13 15:51:55 -0300

    feat: enhance card collection view with empty state and navigation to store

--- apps/www/src/modules/colection/cards.tsx
+++ apps/www/src/modules/colection/cards.tsx
@@ -5,6 +5,8 @@ import { Metadata } from 'next'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { TcgCard } from '@/components/tcg-card-view'
+import Link from 'next/link'
+import { ExternalLink } from 'lucide-react'
 
 type CardItem = {
     id: string
@@ -41,16 +43,28 @@ export function Cards({ data, currentPage, totalPages, search }: InventoryProps)
         <>
             <div className="container mx-auto px-4 py-8">
                 <div className=' flex justify-between items-center'>
-                    <h1 className="text-3xl font-bold mb-6">Sua coleção</h1>
+                    <h1 className="text-3xl font-bold font-syne mb-6">Sua coleção</h1>
                     <form className="flex items-center">
                         <Input type="search" defaultValue={search ?? ''} name="search" placeholder="Pesquisar" className="mr-4" />
                         <Button type="submit">Pesquisar</Button>
                     </form>
                 </div>
+                {data.length === 0 && (
+                    <div className="flex flex-col items-center w-full h-96 font-syne">
+                        <h1 className="text-2xl text-center font-syne">Nenhuma carta encontrada</h1>
+                        <h3 className="text-lg mt-4 text-center font-syne">Acesse a loja para adquirir novas cartas!</h3>
+                        <Button asChild size={'lg'} className=' mt-6'>
+                            <Link href={`/loja`}>
+                                Ir para a loja
+                                <ExternalLink />
+                            </Link>
+                        </Button>
+                    </div>
+                )}
                 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mb-8">
                     {data.map((card) => <TcgCard url={card.image_url} key={card.id} />)}
                 </div>
-                <Pagination>
+                {data.length > 0 && <Pagination>
                     <PaginationContent className=' gap-0'>
                         <PaginationItem>
                             <PaginationPrevious href={currentPage > 1 ? addParam('page', (currentPage - 1).toString()) : '#'} />
@@ -74,7 +88,7 @@ export function Cards({ data, currentPage, totalPages, search }: InventoryProps)
                             <PaginationNext href={currentPage < totalPages ? addParam('page', (currentPage + 1).toString()) : '#'} />
                         </PaginationItem>
                     </PaginationContent>
-                </Pagination>
+                </Pagination>}
             </div>
         </>
     )
