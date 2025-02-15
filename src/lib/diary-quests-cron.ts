b47f36b5bbe6d63b4e003afb2e32d5abc9b29e25
CaioHVectorA <caihebatista@gmail.com>
2025-02-15 02:01:22 -0300

    feat: implement daily cron job for updating diary quests

---
+++ src/lib/diary-quests-cron.ts
@@ -0,0 +1,35 @@
+import { Patterns, type CronConfig } from "@elysiajs/cron";
+import { prisma } from "../helpers/prisma.client";
+
+export function DiaryQuestsCron(): CronConfig {
+  return {
+    name: "Diary Quests",
+    pattern: Patterns.EVERY_DAY_AT_10AM,
+    async run() {
+      const candidates = await prisma.quest.findMany({
+        where: { isDiary: true, isDiaryActive: false },
+      });
+      await prisma.quest.updateMany({
+        where: { isDiaryActive: true },
+        data: {
+          isDiaryActive: false,
+        },
+      });
+      for (let i = 0; i < 3; i++) {
+        const randomIndex = Math.floor(Math.random() * candidates.length);
+        const quest = candidates[randomIndex];
+        await prisma.quest.update({
+          where: { id: quest.id },
+          data: {
+            isDiaryActive: true,
+          },
+        });
+      }
+
+      console.table({
+        message: "Diary Quests updated",
+        date: new Date().toLocaleString(),
+      });
+    },
+  };
+}
