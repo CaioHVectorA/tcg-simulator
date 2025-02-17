18d88c364b79947de22c8318cfed417e72a136a9
CaioHVectorA <caihebatista@gmail.com>
2025-02-17 01:17:04 -0300

    Remove other components

--- apps/www/src/modules/social/index.tsx
+++ apps/www/src/modules/social/index.tsx
@@ -3,11 +3,6 @@ import { useApi } from "@/hooks/use-api"
 import { Loader } from "@/components/loading-spinner"
 import { useQuery } from "@tanstack/react-query"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { FriendList } from "./friends-list"
-import { FriendRequests } from "./friend-requests"
-import { MessageBox } from "./message-box"
-import { MoneyTransfer } from "./money-transfer"
-import { PrivateTradeRequest } from "./private-trade-request"
 
 export type Friend = {
     id: number
@@ -60,32 +55,10 @@ export function Social({
     }
 
     return (
-        <div className="w-full max-w-4xl mx-auto p-4">
-            <Tabs defaultValue="friends" className="w-full">
-                <TabsList className="grid w-full grid-cols-5">
-                    <TabsTrigger value="friends">Friends</TabsTrigger>
-                    <TabsTrigger value="requests">Requests</TabsTrigger>
-                    <TabsTrigger value="messages">Messages</TabsTrigger>
-                    <TabsTrigger value="transfer">Transfer</TabsTrigger>
-                    <TabsTrigger value="trade">Trade</TabsTrigger>
-                </TabsList>
-                <TabsContent value="friends">
-                    <FriendList online={data.online} offline={data.offline} />
-                </TabsContent>
-                <TabsContent value="requests">
-                    <FriendRequests sent={data.sent} received={data.received} />
-                </TabsContent>
-                <TabsContent value="messages">
-                    <MessageBox />
-                </TabsContent>
-                <TabsContent value="transfer">
-                    <MoneyTransfer />
-                </TabsContent>
-                <TabsContent value="trade">
-                    <PrivateTradeRequest />
-                </TabsContent>
-            </Tabs>
-        </div>
+        <>
+            <h1>Should</h1>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </>
     )
 }
 
