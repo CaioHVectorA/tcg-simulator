8f1052cb10efe390a9b6311fa6f29f6e6c3c3a7e
CaioHVectorA <caihebatista@gmail.com>
2025-03-06 17:27:52 -0300

    feat: Wrap Social component in a div for improved layout and styling

--- apps/www/src/app/(app)/social/page.tsx
+++ apps/www/src/app/(app)/social/page.tsx
@@ -1,3 +1,9 @@
 import { Social } from "@/modules/social";
 
-export default () => <Social />
+export default () => {
+    return (
+        <div className=" mt-8 lg:w-[40%] mx-auto">
+            <Social />
+        </div>
+    )
+}
